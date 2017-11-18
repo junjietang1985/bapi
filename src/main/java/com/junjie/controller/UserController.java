bb80b1bb3f1331aa985ea2cbb812d9256839fe3c
junjietang1985 <junjie.tang.1985@gmail.com>
2017-11-18 21:59:38 +0100

    use path variable instead of request parameter

--- src/main/java/com/junjie/controller/UserController.java
+++ src/main/java/com/junjie/controller/UserController.java
@@ -3,17 +3,18 @@ package com.junjie.controller;
 import com.junjie.model.User;
 import com.junjie.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     UserService userService;
 
-    @RequestMapping("/user")
-    public User getUser(@RequestParam long id){
-        return userService.getUser(id);
+    @RequestMapping(method = RequestMethod.GET, value = "/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public User getUser(@PathVariable long userId){
+        return userService.getUser(userId);
     }
 }
