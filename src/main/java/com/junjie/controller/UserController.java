981519cb7d907946f526f3d1320bbec4c69237b6
junjietang1985 <junjie.tang.1985@gmail.com>
2017-11-22 21:47:06 +0100

    save/replace/delete user

--- src/main/java/com/junjie/controller/UserController.java
+++ src/main/java/com/junjie/controller/UserController.java
@@ -31,5 +31,24 @@ public class UserController {
         return this.getUser(newUser.getId());
     }
 
-    //TODO in memory db does not work
+    @RequestMapping(method = RequestMethod.PUT, value = "/{userId}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public User replaceUser(@PathVariable long userId, @RequestBody User user){
+        user.setId(userId);
+        User updatedUser= userService.saveUser(user);
+        return this.getUser(updatedUser.getId());
+    }
+
+//    public User updateUser(){
+//
+//    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{userId}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public User deleteUser(@PathVariable long userId){
+        User user = userService.getUser(userId);
+        user.setDeleted(true);
+        return userService.saveUser(user);
+    }
+
 }
--- src/main/java/com/junjie/service/UserService.java
+++ src/main/java/com/junjie/service/UserService.java
@@ -1,17 +1,13 @@
 package com.junjie.service;
 
-import com.junjie.dao.UserDao;
 import com.junjie.model.User;
 import com.junjie.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import static org.apache.tomcat.jni.Mmap.delete;
 
 @Service
 public class UserService {
-    @Autowired
-    UserDao userDao;
 
     @Autowired
     UserRepository userRepository;
@@ -21,10 +17,6 @@ public class UserService {
     }
 
     public User saveUser(User user){
-        return userDao.upsert(user);
-    }
-
-    public User deleteUser(long id){
-        return userDao.delete(id);
+        return userRepository.save(user);
     }
 }
